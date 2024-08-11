f572b6cf8d8be8336c9fff7f12ee8e221cef4b95
Jonathan <johuesing99@gmail.com>
2024-08-11 09:33:46 +0200

    Add Footer for Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-    const loggedIn = { firstName: 'Jonathan', lastName: 'HUE', email: 'johuesing99@gmail.com'}
+const Home = async () => {
+    const loggedIn = await getLoggedInUser();
     return (
         <div className="home">
             <div className="home-content">
@@ -12,7 +13,7 @@ const Home = () => {
                     <HeaderBox
                         type="greeting"
                         title="Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
+                        user={loggedIn?.name || 'Guest'}
                         subtext="Access and manage your account and transactions from here."
                     />
 
